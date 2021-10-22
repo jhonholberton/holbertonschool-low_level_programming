abf79626aa7a34e62c95b4ccba805b9be9b6364c
jhonholberton <3425@holbertonschool.com>
2021-10-22 12:48:49 -0500

    Write a program that prints its name, followed by a new line.

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 int _putchar(char c);
-int main (int argc, char *argv[]);
-int main (int argc, char **argv);
+int main(int argc, char *argv[]);
+int main(int argc, char **argv);
 
 #endif
