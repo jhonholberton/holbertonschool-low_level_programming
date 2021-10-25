0d4daa97ce5d98f92f6db1016a4c936b13be8149
jhonholberton <3425@holbertonschool.com>
2021-10-25 17:56:26 -0500

    Revert "Create the static library libmy.a containing all the functions listed below"
    This reverts commit 947e1c6af9ed05ae10619b99d4631115dafb49c6.

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_HEADER
-#define MAIN_HEADER
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,4 +26,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /*MAIN_HEADER*/
+#endif /*MAIN_H*/
