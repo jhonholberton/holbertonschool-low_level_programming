2ba2965bd2c7c8bbb4749c4c8f5fab9cb5159c5f
jhonholberton <3425@holbertonschool.com>
2021-11-04 16:00:51 -0500

    fix sum_them

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - Write a function that prints anything.
+ * @format: list of types of arguments
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	
+}
