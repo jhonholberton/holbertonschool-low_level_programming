5686a2076b646bdc9b7ecf62b51e63638fd8c81f
jhonholberton <3425@holbertonschool.com>
2021-11-03 22:55:07 -0500

    Write a program that performs simple operations.

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -4,6 +4,7 @@
  * op_add - add
  * @a: variable
  * @b: variable
+ * Return: a + b
  */
 int op_add(int a, int b)
 {
@@ -13,6 +14,7 @@ int op_add(int a, int b)
  * op_sub - sub
  * @a: variable
  * @b: variable
+ *  Return: a - b
  */
 int op_sub(int a, int b)
 {
@@ -22,6 +24,7 @@ int op_sub(int a, int b)
  * op_mul - mul
  * @a: variable
  * @b: variable
+ *  Return: a * b
  */
 int op_mul(int a, int b)
 {
@@ -31,6 +34,7 @@ int op_mul(int a, int b)
  * op_div - div
  * @a: variable
  * @b: variable
+ *  Return: a / b
  */
 int op_div(int a, int b)
 {
@@ -46,6 +50,7 @@ int op_div(int a, int b)
  * op_mod - mod
  * @a: variable
  * @b: variable
+ *  Return: a % b
  */
 int op_mod(int a, int b)
 {
