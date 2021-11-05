12e2e2d5804f97245b82c42db73a7dd176756b3e
jhonholberton <3425@holbertonschool.com>
2021-11-04 22:27:37 -0500

    Write a function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,5 +7,40 @@
  */
 void print_all(const char * const format, ...)
 {
-	
+	int i = 0;
+	char *p;
+	va_list j;
+
+	va_start(j, format);
+	if (!format)
+	{
+		printf("\n");
+		return;
+	}
+	while (format[i])
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(j, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(j, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(j, double));
+			break;
+		case 's':
+			p = va_arg(j, char *);
+			p ? printf("%s", p) : printf("(nil)");
+			break;
+		default:
+			i++;
+			continue;
+		}
+		i++;
+		if (format[i] != '\0')
+			printf(", ");
+	}
+	printf("\n");
 }
